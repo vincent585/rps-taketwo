8db4c2b366c79467a65e79e2de33d2c201312f88
Vincent Klimjack <vklimjack@gmail.com>
2024-02-28 15:10:36 +0000

    add playRound function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    let choiceIndex = Math.floor(Math.random() * choices.length)
+    let choiceIndex = Math.floor(Math.random() * choices.length);
 
-    return choices[choiceIndex]
+    return choices[choiceIndex];
+}
+
+function playRound(playerChoice, computerChoice) {
+    playerChoice = playerChoice.toLowerCase();
+
+    if (playerChoice === computerChoice) {
+        return `You both picked ${playerChoice} - It's a tie!`;
+    } else if (playerChoice === "rock") {
+        return computerChoice === "scissors" ? "Rock smashes scissors - you win!" : "Paper covers rock - you lose!";
+    } else if (playerChoice === "scissors") {
+        return computerChoice === "paper" ? "Scissors cut paper - you win!" : "Rock smashes scissors - you lose!";
+    } else if (playerChoice === "paper") {
+        return computerChoice === "rock" ? "Paper covers rock - you win!" : "Scissors cut paper - you lose!";
+    }
 }
