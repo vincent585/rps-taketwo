7e2f9cea69429a14e965aa9c6cc6f0c581701e22
Vincent Klimjack <vklimjack@gmail.com>
2024-03-05 08:18:11 +0000

    add replay functionality

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ const selectionOptions = document.querySelectorAll(".selection-options button");
 const playerScorePara = document.querySelector(".scoreboard p:nth-child(1)");
 const computerScorePara = document.querySelector(".scoreboard p:nth-child(2)");
 const roundResult = document.querySelector(".round-result");
-const selectionDiv = document.querySelector('.selection-options');
+const selectionDiv = document.querySelector(".selection-options");
+const gameContainer = document.querySelector(".game-container");
 
 selectionOptions.forEach((selectionOption) => selectionOption.addEventListener("click", roundHandler));
 
@@ -94,8 +95,16 @@ function endGame() {
     selectionOptions.forEach((selectionOption) => selectionDiv.removeChild(selectionOption));
     const replayBtn = document.createElement("button");
     const replayImg = document.createElement("img");
+    const playAgain = document.createElement("p");
+    playAgain.textContent = "Play again?";
     replayBtn.appendChild(replayImg);
     replayImg.setAttribute("src", "./images/replay.png");
     replayImg.setAttribute("alt", "replay icon");
     selectionDiv.appendChild(replayBtn);
+    replayBtn.addEventListener("click", resetGame);
+    gameContainer.appendChild(playAgain);
+}
+
+function resetGame() {
+    location.reload();
 }
